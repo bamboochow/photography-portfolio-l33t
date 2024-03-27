2c1a1b28cc5a81b47c6ab395bdc136c9f60ac896
Paul Chris Luke <paulchrisluke@gmail.com>
2024-03-27 14:04:10 +0800

    Initial commit

---
+++ src/components/Featured.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Featured = () => {
+  return (
+    <div className="grid gap-4 mt-16 md:mt-20">
+      <div>
+        <video className="rounded-lg" controls autoPlay muted>
+          <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="grid grid-cols-5 gap-4">
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
---
+++ src/components/Masonry.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Masonry = () => {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid gap-4">
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Masonry;
