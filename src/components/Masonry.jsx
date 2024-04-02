89a30cf0ccc4237251346a04c667e421b366cb56
Paul Chris Luke <paulchrisluke@gmail.com>
2024-04-02 18:18:49 +0800

    fix masonry

--- src/components/Masonry.jsx
+++ src/components/Masonry.jsx
@@ -3,71 +3,87 @@ import React from 'react';
 const Masonry = () => {
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      {/* Column 1 */}
       <div className="grid gap-4">
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+          {/* Column 1 Image 1 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="" />
+          {/* Column 1 Image 2 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+          {/* Column 1 Image 3 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050361/Featured/424427755_700215432294133_7926375486699430767_n_zd03hi.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050361/Featured/424427755_700215432294133_7926375486699430767_n_zd03hi.webp" alt="" />
           </a>
         </div>
       </div>
+      {/* Column 2 */}
       <div className="grid gap-4">
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+          {/* Column 2 Image 1 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+          {/* Column 2 Image 2 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="" />
+          {/* Column 2 Image 3 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" alt="" />
           </a>
         </div>
       </div>
+      {/* Column 3 */}
       <div className="grid gap-4">
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+          {/* Column 3 Image 1 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712049978/Featured/434507367_415382937786431_1923301509234082131_n_kstcpt.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712049978/Featured/434507367_415382937786431_1923301509234082131_n_kstcpt.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="" />
+          {/* Column 3 Image 2 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+          {/* Column 3 Image 3 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" alt="" />
           </a>
         </div>
       </div>
+      {/* Column 4 */}
       <div className="grid gap-4">
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+          {/* Column 4 Image 1 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="" />
+          {/* Column 4 Image 2 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" alt="" />
           </a>
         </div>
         <div>
-          <a href="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+          {/* Column 4 Image 3 */}
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/434516104_1147418039933313_4362840665304610784_n_nnbo5n.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/434516104_1147418039933313_4362840665304610784_n_nnbo5n.webp" alt="" />
           </a>
         </div>
       </div>
