c62c54c20230f6ffcbeac183350d6c8f95d4dde5
Paul Chris Luke <paulchrisluke@gmail.com>
2024-04-02 17:10:42 +0800

    change video

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,18 +4,8 @@ import { Container } from './Container'
 const Featured = () => {
   return (
     <div className="mt-16 grid gap-4 md:mt-20">
-      <div className="relative">
-        <img
-          src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712047955/Header/VS--YouTube--0_01_1_sfvfmk.webp"
-          alt="Video Thumbnail"
-          className="cursor-pointer rounded-lg"
-        />
-        <video
-          className="absolute inset-0 h-full w-full rounded-lg"
-          controls
-          autoPlay
-          muted
-        >
+      <div>
+        <video className="rounded-lg" controls autoPlay muted>
           <source
             src="https://res.cloudinary.com/dxt5dbdan/video/upload/v1712042431/Mountain_Lin_Train_wvhr3b.mp4"
             type="video/mp4"
