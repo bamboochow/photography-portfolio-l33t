325a010c8fb2db9271f7a01579268cb597f50b8d
Paul Chris Luke <paulchrisluke@gmail.com>
2024-04-02 16:36:25 +0800

    resolve conflicts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,91 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import clsx from 'clsx'
-
-import { Button } from '@/components/Button'
-import { Card } from '@/components/Card'
+// import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { FacebookIcon, InstagramIcon } from '@/components/SocialIcons'
-import { getAllArticles } from '@/lib/articles'
-import { formatDate } from '@/lib/formatDate'
 import Featured from '@/components/Featured'
 import Masonry from '@/components/Masonry'
 import { Timeline } from '@/components/Timeline'
+import avatarImage from '@/images/avatar.jpg'
 
-function MailIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function BriefcaseIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
-
-function ArrowDownIcon(props) {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  )
-}
-
-function Article({ article }) {
-  return (
-    <Card as="article">
-      <Card.Title href={`/articles/${article.slug}`}>
-        {article.title}
-      </Card.Title>
-      <Card.Eyebrow as="time" dateTime={article.date} decorate>
-        {formatDate(article.date)}
-      </Card.Eyebrow>
-      <Card.Description>{article.description}</Card.Description>
-      {/* <Card.Cta>Read article</Card.Cta> */}
-    </Card>
-  )
-}
 
 function SocialLink({ icon: Icon, ...props }) {
   return (
@@ -95,13 +18,18 @@ function SocialLink({ icon: Icon, ...props }) {
   )
 }
 
-export default async function Home() {
-  let articles = (await getAllArticles()).slice(0, 4)
-
+export default function Home() {
   return (
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
+        <Image
+        src={avatarImage}
+        alt=""
+        className=
+          'my-6 h-16 w-16 rounded-full bg-zinc-100 object-cover dark:bg-zinc-800'
+        priority
+      />
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             Lin Meng Shan AKA Mountain 林盟山
           </h1>
@@ -129,11 +57,6 @@ export default async function Home() {
               aria-label="Follow on Facebook"
               icon={FacebookIcon}
             />
-            {/* <SocialLink
-              href="#"
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            /> */}
           </div>
         </div>
       </Container>
@@ -142,11 +65,8 @@ export default async function Home() {
       </Container>
       <Container>
         <h2 class="mt-4 text-2xl font-bold text-zinc-100 sm:text-xl">
-          影片說明：
+        The Non-Existing Railroad Sceneries:
         </h2>
-        <p class="mt-4 text-zinc-400">
-          Title: The Non-Existing Railroad Sceneries
-        </p>
         <p class="mt-4 text-zinc-400">
           Is it landscape or figures paintings on the train? Or the non-existing
           figures paintings on the train.
--- src/lib/formatDate.js
+++
@@ -1,3 +0,0 @@
-export function formatDate(dateString) {
-  return new Date(`${dateString}T00:00:00Z`).getFullYear().toString();
-}
