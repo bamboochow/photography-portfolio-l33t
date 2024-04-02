27b1b1048c087d4734a49026caa84158c9715599
Paul Chris Luke <paulchrisluke@gmail.com>
2024-04-02 18:26:40 +0800

    fix masonry order

--- src/components/Masonry.jsx
+++ src/components/Masonry.jsx
@@ -7,14 +7,14 @@ const Masonry = () => {
       <div className="grid gap-4">
         <div>
           {/* Column 1 Image 1 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" alt="" />
           </a>
         </div>
         <div>
           {/* Column 1 Image 2 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050434/Featured/424566330_635304952027462_7851072009938144943_n_hyabtp.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050574/Featured/183429039_834991267406178_4426577820415001890_n_mfaqeg.webp" alt="" />
           </a>
         </div>
         <div>
@@ -28,20 +28,20 @@ const Masonry = () => {
       <div className="grid gap-4">
         <div>
           {/* Column 2 Image 1 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" alt="" />
           </a>
         </div>
         <div>
           {/* Column 2 Image 2 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050261/Featured/434730666_331675809890501_4409593087573074263_n_evxojh.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050261/Featured/434730666_331675809890501_4409593087573074263_n_evxojh.webp" alt="" />
           </a>
         </div>
         <div>
           {/* Column 2 Image 3 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050303/Featured/434527182_376315915224961_2287386279992503209_n_phg1vz.webp" alt="" />
           </a>
         </div>
       </div>
@@ -55,14 +55,14 @@ const Masonry = () => {
         </div>
         <div>
           {/* Column 3 Image 2 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712050199/Featured/434521881_790955833079560_7044666438236520662_n_qvoqtd.webp" alt="" />
           </a>
         </div>
         <div>
           {/* Column 3 Image 3 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048948/Featured/17076104_1868637090051838_5974515184813735936_n_qe8h8a.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048949/Featured/17077705_168502223657228_8871044221612589056_n_eakngw.webp" alt="" />
           </a>
         </div>
       </div>
@@ -70,14 +70,14 @@ const Masonry = () => {
       <div className="grid gap-4">
         <div>
           {/* Column 4 Image 1 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" alt="" />
           </a>
         </div>
         <div>
           {/* Column 4 Image 2 */}
-          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" target="_blank" rel="noopener noreferrer">
-            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048390/Featured/126898455_373986903835665_7625357430987327025_n_maazkg.webp" alt="" />
+          <a href="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" target="_blank" rel="noopener noreferrer">
+            <img className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dxt5dbdan/image/upload/v1712048391/Featured/434624062_1027080969009248_3728897588784353769_n_k1w6z0.webp" alt="" />
           </a>
         </div>
         <div>
